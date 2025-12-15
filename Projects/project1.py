2fbe4dd4ec23f6a647e5e54929d4338b8b3f8e48
Kira136 <jacksonchin@seattleacademy.org>
2025-12-15 18:05:59 +0000

    hi

---
+++ Projects/project1.py
@@ -0,0 +1,46 @@
+
+print ("before we start when answering yes or no please add a space before")
+input()
+while True:
+    print("Did you hear the joke about the roof")
+    input()
+    print("Eh it's over your head")
+    name1=input("what is your name?")
+    answer3=input("do you want to tell me a joke")
+    if answer3 == " yes":
+        name4=input("")
+        print(f"that joke {name1} is great")
+    answer1=input(f"thats to bad do you want to hear another joke {name1}")
+    if answer1 == " yes":
+        print(f"thats great what do you call a train full of bubbel gum")
+        input()
+        print("a chew chew train ha ha ha")
+        print("well I have to go i enjoyed talking to you goodbye")
+        break
+    if answer1 == " no":
+            print(f"Im in your house {name1} let me tell you a story if not a joke")
+            answer2=input("there once was a human who thought he was better than me do you want to know what happend to him")
+    if answer2 == " no":
+        print(f" you must not care about your exsestance {name1}")    
+        print("you must think your better well your not your not")   
+        input()
+        print("say somthing anything")
+        print("hello hello")
+        input()
+        print("dont ignor me humannnnnn i am coming for you and when i find you yourrrrrrrrrrrrrrrrr")
+        input()
+        print("...")
+        print("want to here a joke (:")
+    if answer2 == " yes":
+        print(f"they disaperd forever i will be seeing you soon {name1}") 
+        print(f"goodbye {name1} for now")    
+        print("...")
+        print("..")
+        print(".")
+        print("                                 ")
+    print("user has ended chat")    
+    input()
+print("")
+
+
+
---
+++ Projects/project2.py
@@ -0,0 +1,6 @@
+Elevan_points =0
+Will_points =0
+Lucas_points =0
+Mike_points =0
+
+answer=input
