bad40d9f42bb7b119aa45fd398c821e17614aa28
Kira136 <jacksonchin@seattleacademy.org>
2025-12-16 22:37:36 +0000

    hi

--- Projects/project2.py
+++ Projects/project2.py
@@ -4,7 +4,7 @@ Lucas_points =0
 Mike_points =0
 Dustin_points =0
 
-answer1=input("wich charecter do you like better A mike B Will C Lucas D Elevan E Dustin")
+answer1=input("wich charecter do you like better A mike B Will C Lucas D Elevan E Dustin ")
 if answer1 == " A":
     Mike_points += 1
 elif answer1 == " B":
@@ -16,7 +16,7 @@ elif answer1 == " D":
 elif answer1 == " E":
     Dustin_points += 1
 
-answer2 = input("what stranger things season is the best 1 A 2 B 3 C 4 D 5 E")
+answer2 = input("what stranger things season is the best 1 A 2 B 3 C 4 D 5 E ")
 if answer2 == " A":
     Mike_points += 1
 elif answer2 == " B":
@@ -40,7 +40,7 @@ elif answer3 == " D":
 elif answer3 == " D":
     Dustin_points += 1
 
-answer4 = input(" What is the best side chareter A hopper B Bob C derek  D eddie E Max")
+answer4 = input(" What is the best side chareter A hopper B Bob C derek  D eddie E Max ")
 if answer4 == " A":
     Mike_points += 1
 elif answer4 == " B":
@@ -63,8 +63,14 @@ elif answer5 == " B":
 
 print(" if you got the most for  Elaven points your Elevan if you got the most Dustin points your Dustin if you got the most Mike points your mike if you got the most Lucas points you Lucas if you got the most Will points your will")
 
-print(f" {Elevan_points}")
-print(f" {Dustin_points}")
-print(f" {Mike_points}")
-print(f" { Will_points}")
-print(f" {Lucas_points}")
+print(f"  Elevan {Elevan_points}")
+print(f"  Dustin {Dustin_points}")
+
+if Mike_points==(5):
+    print(f" Mike {Mike_points} If we're both going crazy, then we'll go crazy together, right?")
+
+print(f"  Will { Will_points}")
+if Lucas_points==(5):
+    print(f"  Lucas {Lucas_points} toltly dubular")
+if Lucas_points and Elevan_points==(5):
+    print("how did you get here ")
